8b05ad5c542bb7f2b52b74168493066ccc2b5823
2Aram1Hakobyan2 <ar2hakobyan1@yahoo.com>
2023-07-04 20:19:39 +0400

    changer were madde.

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,7 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a=1,b=1;
-        int x = (9*a^2-5*b+2+a-7)*((a+b-4*a*b)/2);
+        // Declare variables
+        double a = 2.5; // Example value for variable a
+        double b = 1.8; // Example value for variable b
+
+        // Calculate the result using the formula
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        // Print the result
+        System.out.println("The result is: " + result);
     }
 }
